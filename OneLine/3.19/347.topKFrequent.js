2981064e36587ccd5f1fcc300d41bc6b1d37d2e3
OneLine <49432399+Lu1Hang@users.noreply.github.com>
2021-03-19 13:59:54 +0800

    Create 347.topKFrequent.js

---
+++ OneLine/3.19/347.topKFrequent.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    var hashmap= {};
+    for (const num of nums){
+        hashmap[num] == undefined? hashmap[num] = 1:hashmap[num]++;
+    }
+    return Object.keys(hashmap).sort((a,b)=> hashmap[b]-hashmap[a]).slice(0,k);
+
+};
+
