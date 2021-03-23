6f1093917d95011424a1e52769c8f6542c775c58
OneLine <49432399+Lu1Hang@users.noreply.github.com>
2021-03-23 15:06:16 +0800

    Create 153.findMin.js

---
+++ OneLine/3.23/153.findMin.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    var left = 0, right = nums.length-1;
+    if (nums[right] > nums[0]) return nums[0];
+    while (left < right)
+    {
+        var mid = Math.floor(left + (right-left)/2);
+        if (nums[mid-1] > nums[mid]) return nums[mid];
+        if (nums[mid+1] < nums[mid]) return nums[mid+1];
+        if (nums[mid] > nums[0])
+        {
+            left = mid +1;
+        }
+        else right = mid-1;
+    }
+    return nums[left];
+};
