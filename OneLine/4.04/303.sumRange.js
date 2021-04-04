cbea41f5a4882bcd55b3e3f5081a1994fac23c3f
OneLine <49432399+Lu1Hang@users.noreply.github.com>
2021-04-04 16:49:33 +0800

    Create 303.sumRange.js

---
+++ OneLine/4.04/303.sumRange.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    const len = nums.length;
+    this.sums = new Array(len+1).fill(0);
+
+    for (let i=0;i<len;i++)
+    {
+        this.sums[i+1]=this.sums[i] + nums[i];
+    }
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    return this.sums[right+1] - this.sums[left];
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
